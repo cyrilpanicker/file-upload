accce2be8034874876a06598f7b056db200f5261
cyrilpanicker <cyrilpanicker@gmail.com>
2017-05-19 17:02:15 +0530

    added changes

--- client/src/app.js
+++ client/src/app.js
@@ -1,10 +1,51 @@
 import Vue from 'vue';
+import axios from 'axios';
+
+var components = {};
+
+components['file-upload'] = {
+    props:['label','multiple','url'],
+    methods:{
+        upload:function(){
+            var data = new FormData();
+            var files = this.$refs['fileInput'].files;
+            var config = {
+                onUploadProgress : function(progressEvent){
+                    var percentCompleted = Math.round(progressEvent.loaded*100/progressEvent.total);
+                    console.log(percentCompleted);
+                }
+            };
+            for(var i=0;i<files.length;i++){
+                data.append('test',files[i]);
+            }
+            axios.put(this.url,data,config).then(
+                function(response){
+                    console.log(response.data);
+                },function(error){
+                    console.log(error);
+                }
+            );
+        }
+    },
+    template:`
+        <form onsubmit="return false;">
+            <label for="file">{{label}}</label>
+            <input ref="fileInput" id="file" :multiple="multiple" type="file" />
+            <button @click="upload">Upload</button>
+        </form>
+    `
+};
 
 var app = new Vue({
-    template:`<div>{{text}}</div>`,
-    data:{
-        text:'cyril'
-    }
+    components:components,
+    template:`
+        <file-upload
+            label="file"
+            :multiple="true"
+            url="file"
+        ></file-upload>
+    `,
+    data:{}
 });
 
 app.$mount('#app');
--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var multer = require('multer');
 
+var upload = multer({dest:'uploads/'});
 var app = express();
 
 app.use(bodyParser.json());
-
 app.use(express.static('./client/dist'));
 
-app.post('/test',(request,response) => {
-    response.send(request);
+app.put('/file',upload.array('test'),function(request,response){
+    response.send(
+        request.files.map(function(file){
+            return {
+                fileName:file.filename,
+                originalFileName:file.originalname
+            };
+        })
+    );
 });
 
 var ipaddress = '127.0.0.1';
