13dd6e8cdf528f90a44dc504d5dee0fd8f2c482d
cyrilpanicker <cyrilpanicker@gmail.com>
2017-05-30 16:51:56 +0530

    added changes

--- client/src/app.js
+++ client/src/app.js
@@ -8,21 +8,25 @@ components['file-upload'] = {
     methods:{
         upload:function(){
             var data = new FormData();
-            var files = this.$refs['fileInput'].files;
+            var inputRef = this.$refs['fileInput'];
+            var files = inputRef.files;
             var config = {
                 onUploadProgress : function(progressEvent){
                     var percentCompleted = Math.round(progressEvent.loaded*100/progressEvent.total);
                     console.log(percentCompleted);
                 }
             };
+            var self = this;
             for(var i=0;i<files.length;i++){
                 data.append('files',files[i]);
             }
             axios.put(this.url,data,config).then(
                 function(response){
-                    console.log(response.data);
+                    inputRef.value = null;
+                    self.$emit('upload');
                 },function(error){
                     console.log(error);
+                    self.$emit('upload');
                 }
             );
         }
@@ -36,16 +40,42 @@ components['file-upload'] = {
     `
 };
 
+components['file-list'] = {
+    props:['list'],
+    template:`
+        <ul>
+            <li v-for="file in list">
+                <a :href="file.metadata.fileLink" target="_blank">{{file.metadata.originalFileName}}</a>
+            </li>
+        </ul>
+    `
+};
+
 var app = new Vue({
     components:components,
+    data:{
+        fileList:[]
+    },
+    methods:{
+        getFiles:function(){
+            var self = this;
+            axios.get('files').then(
+                function(response){
+                    self.fileList = response.data;
+                    console.log(response.data);
+                },function(error){
+                    console.log(error);
+                }
+            );
+        }
+    },
     template:`
-        <file-upload
-            label="file"
-            :multiple="true"
-            url="files"
-        ></file-upload>
-    `,
-    data:{}
+        <div class="app">
+            <file-upload @upload="getFiles" label="" :multiple="true" url="files"></file-upload>
+            <file-list :list="fileList"></file-list>
+        </div>
+    `
 });
 
 app.$mount('#app');
+app.getFiles();
--- server/app.js
+++ server/app.js
@@ -2,16 +2,37 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var multer = require('multer');
 var mongo = require('mongodb');
-var Grid = require('gridfs-stream');
-var multerConfig = require('./multer-config');
+var grid = require('gridfs-stream');
+var multerGfsStorage = require('multer-gridfs-storage');
+
+var fileLocation = 'http://localhost:8000/file/';
+var timeStampedFileName = '';
 
 var db = new mongo.Db('photum', new mongo.Server("127.0.0.1", 27017));
-db.open(function (error) {
+db.open(function (error,db) {
     if (error){
         console.log('db-connection-error');
+        return;
     }
-    var gfs = Grid(db, mongo);
-    var upload = multer({storage:multerConfig.fileStorage});
+    var gfs = grid(db, mongo);
+    var upload = multer({
+        storage:multerGfsStorage({
+            gfs:gfs,
+            filename:function(request,file,callback){
+                callback(null,timeStampedFileName);
+            },    
+            metadata:function(request,file,callback){
+                callback(null,{
+                    originalFileName:file.originalname,
+                    fileLink:fileLocation+encodeURIComponent(timeStampedFileName)
+                });
+            }
+        }),
+        fileFilter:function(request,file,callback){
+            timeStampedFileName = getTimeStampedFileName(file.originalname);
+            callback(null,true);
+        }
+    });
     var app = express();
     app.use(bodyParser.json());
     app.use(express.static('./client/dist'));
@@ -21,10 +42,17 @@ db.open(function (error) {
         }));
     });
     app.get('/file/:fileName',function(request,response){
-        console.log(request.params.fileName);
         var gfsStream = gfs.createReadStream({filename:request.params.fileName});
         gfsStream.pipe(response);
-        // response.send(request.params.fileName);
+    });
+    app.get('/files',function(request,response){
+        db.collection('fs.files').find().toArray(function(error,files){
+            if(error){
+                response.sendStatus(500).send({error:'db-error'});
+            }else{
+                response.send(files);
+            }
+        });
     });
     var ipaddress = '127.0.0.1';
     var port = 8000;
@@ -32,3 +60,19 @@ db.open(function (error) {
         console.log('listening at : http://'+ipaddress+':'+port);
     });
 });
+
+function getTimeStampedFileName(fileName){
+    var fileExtension = '';
+    var splitFileName = fileName.split('.');
+    if(splitFileName.length>1){
+        fileExtension = '.'+splitFileName.pop();
+    }
+    var resultFileName = '';
+    if(!fileExtension){
+        resultFileName = fileName;
+    }else{
+        resultFileName = fileName.replace(new RegExp(fileExtension+'$'),'');
+    }
+    resultFileName = resultFileName + '_' + new Date().getTime() + fileExtension;
+    return resultFileName;
+}
