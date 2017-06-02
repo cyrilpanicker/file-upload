08208047c966d040b4435f4f9b648c66bda4dbc7
cyrilpanicker <cyrilpanicker@gmail.com>
2017-06-02 23:47:02 +0530

    added changes

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,11 @@ module.exports = {
         }),
         new webpack.optimize.CommonsChunkPlugin({
             name:['app','libs']
+        }),
+        new webpack.DefinePlugin({
+            'process.env':{
+                'NODE_ENV':JSON.stringify(process.env.NODE_ENV)
+            }
         })
     ],
     devServer:{
